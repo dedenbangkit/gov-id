ed1919f36e667a60a26b34571759e2b3d04965d9
dedenbangkit <mail@dedenbangkit.com>
2018-02-07 20:05:23 +0700

    add exception and readme files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var ask = require('inquirer');
 var cheerio = require('cheerio');
 var request = require('request');
+var color = require('colors');
 const download = require('download');
 const fs = require('fs');
 const url = 'http://data.go.id';
@@ -22,27 +23,57 @@ function getList(x) {
         let $ = null;
         $ = cheerio.load(body);
         let pages = $('.pagination-centered>ul>li').length - 1;
+        if(pages > -1){
         for (let i = 1; i < pages; i++) {
             request(url + '/dataset?q=' + x + '&page=' + i, function(error, response, html) {
                 $ = null;
                 $ = cheerio.load(html);
                 $('.dataset-heading>a').each(function() {
-                    var link = 'http://data.go.id' + $(this).attr('href');
+                    let link = 'http://data.go.id' + $(this).attr('href');
                     request(link, function(error, response, html) {
                         $ = null;
                         $ = cheerio.load(html);
-                        var file = $('.dropdown-menu>li').last().children().attr('href');
+                        let file = $('.dropdown-menu>li').last().children().attr('href');
                         request(file, function(error, response, html) {
                             if(!error){
                                 download(file, 'download/' + x).then(() => {
-                                    console.log('success: ' + file);
+                                    let title = file.substring(file.lastIndexOf("/") + 1, file.length);
+                                    console.log(color.green('success') + color.white(': ' + title));
                                 });
                             }else{
-                                console.log('data-not-found: ' + file);
+                                console.log(color.red('data not found') + color.white(': ' + file));
                             }
                         });
                     });
                 });
+            });
+        }
+        }else{
+            request(url + '/dataset?q=' + x, function(error, response, html) {
+                $ = null;
+                $ = cheerio.load(html);
+                if ($('.dataset-heading>a') > 0){
+                $('.dataset-heading>a').each(function() {
+                    let link = 'http://data.go.id' + $(this).attr('href');
+                    request(link, function(error, response, html) {
+                        $ = null;
+                        $ = cheerio.load(html);
+                        let file = $('.dropdown-menu>li').last().children().attr('href');
+                        request(file, function(error, response, html) {
+                            if(!error){
+                                download(file, 'download/' + x).then(() => {
+                                    let title = file.substring(file.lastIndexOf("/") + 1, file.length);
+                                    console.log(color.green('success') + color.white(': ' + title));
+                                });
+                            }else{
+                                console.log(color.red('data not found') + color.white(': ' + file));
+                            }
+                        });
+                    });
+                });
+                }else{
+                    console.log(color.red('no data found for ')+color.white(x));
+                }
             });
         }
     });
---
+++ readme.md
@@ -0,0 +1,38 @@
+![logo](http://blog.nusantech.co/wp-content/uploads/2016/10/icon-1.png)
+
+## About
+---
+Gov-ID is created for collecting all available government data which stored on [data.go.id](http://data.go.id). This way much faster to download all the data.
+
+## Usage (CLI)
+---
+### Installation
+```
+git clone https://github.com/dedenbangkit/gov-id
+npm install
+```
+## Execute
+```
+npm run gov-id
+```
+
+## Contributing
+---
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+
+## Packages
+---
+* [Cheerio](https://github.com/cheeriojs/cheerio). Simple implementation of core jQuery designed specifically for the server.
+* [Inquery](https://github.com/SBoudrias/Inquirer.js). A collection of common interactive command line user interfaces.
+* [Request](https://github.com/request/request). Simplified HTTP request client.
+* [Colors](https://github.com/Marak/colors.js). Get colors in your node.js console.
+
+
+## License
+---
+MIT
